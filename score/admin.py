c327f88cb6e0ed984562723f55ea5fbd6c780aa4
muchamucha <edkwcn@sina.com>
2017-02-10 00:16:18 +0800

    Add files via upload

--- score/admin.py
+++ score/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from score.models import worker,Company,Procdure,work_steam,score
+from score.models import worker,Company,Procdure,work_steam,score,test2,left
 # Register your models here.
 
-admin.site.register([worker,Company,Procdure,work_steam,score])
+admin.site.register([worker,Company,Procdure,work_steam,score,test2,left])
--- score/models.py
+++ score/models.py
@@ -2,14 +2,9 @@
 from __future__ import unicode_literals
 from django.contrib import admin
 from django.db import models
+from django.contrib.auth.models import Group
 import datetime
-# Create your models here.
-class User(models.Model):
-	username=models.CharField(max_length=50,primary_key=True)
-	password=models.CharField(max_length=50,default='')
-	group=models.CharField(max_length=50,default="User")
-	def __unicode__(self):
-		return self.username
+
 class Company(models.Model):
 	sn=models.CharField(max_length=100,default="")
 	company=models.CharField(max_length=100,default="")
@@ -44,12 +39,24 @@ class score(models.Model):
 	score=models.IntegerField()
 	def __unicode____(self):
 		return str(self.worker_id)+str(self.score)
-class test(models.Model):
+class test2(models.Model):
 	serial_number=models.CharField(verbose_name=u'编号',max_length=10,default='')
 	name=models.CharField(u'姓名',max_length=10,default='')
 	typ=models.CharField(u'变更类型',max_length=10,default='')
 	remark=models.TextField(verbose_name=u'变更说明', default='')
 	create_time=models.DateTimeField(verbose_name=u'创建时间', default=datetime.datetime.now())
-class nav_link(models.Model):
+class left(models.Model):
+	link_id=models.AutoField(primary_key=True,unique=True)
+	label=models.CharField(max_length=10,default="link")
+	link=models.CharField(max_length=200,default="")
+	pic=models.ImageField(upload_to="static/images/",blank=True)
+	authlevel=models.ManyToManyField(Group,blank=True)
+	parent_id=models.ForeignKey('self',blank=True,null=True,verbose_name=u"父菜单")
+	CHOICES=(("P",u"父菜单"),("S",u"子菜单"))
+	menulevel=models.CharField(max_length=2,verbose_name=u"菜单级别",choices=CHOICES)
+	def __unicode__(self):
+		return self.label
+
+#权限需要一对多
 	
-admin.site.register(User)
+# admin.site.register(User) #当使用score自带用户使使用
